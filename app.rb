934d75f5a9000458aa3508841e9d1842aba65109
FlowTech <admin@FlowTech-LAB.org>
2025-09-25 14:23:32 +0200

    Player.rb terminer

--- app.rb
+++ app.rb
@@ -10,4 +10,10 @@ require_relative 'lib/board'
 
 # Application : cette classe va nous permettre de lancer le jeu. Elle va faire une boucle infinie de parties (on joue tant que les joueurs veulent continuer) et lancer l'instanciation d'un Game.
 
+player1 = Player.new("José")
+player2 = Player.new("John")
+DELAY = 1
+
+#binding.pry
+
 Game.new.run!
--- lib/player.rb
+++ lib/player.rb
@@ -3,4 +3,14 @@
 # Variables d'instance : un joueur a un nom (habile) et un symbole ("x" ou "o").
 
 class Player
+  attr_accessor :name, :symbole
+  @@all_player = []
+  def initialize(name, symbole)
+    @name = name
+    @symbole = symbole
+    @@all_players << self
+  end
+  def self.all
+    @@all_player
+  end
 end
