44d536f0161e333e8ef24715e67572988549ac30
FlowTech <admin@FlowTech-LAB.org>
2025-09-25 12:14:26 +0200

    Add structure

---
+++ app.rb
@@ -0,0 +1,31 @@
+#class Player
+
+
+
+#class BoardCase
+
+#@A1 
+#@A2
+#@A2 = O
+#@A2
+#@A2
+#@A2
+#@A2
+
+
+#class Board
+
+#Coup valable ou resaisis
+
+#Logique win - loose
+
+a2 = " "
+
+#class Show
+
+puts "   1   2   3 "
+puts "A #{a2}  | #{a2} | #{a2} "
+puts "  -----------"
+puts "B #{a2}  | #{a2} | #{a2} "
+puts "  -----------"
+puts "C #{a2}  | #{a2} | #{a2} "
